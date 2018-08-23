af957cee2953ceb938dffa973d585d96508b1e54
han-meim <969437631@qq.com>
2018-08-23 10:32:01 +0800

    修改重塑矩阵

--- matrix_reshape/matrix_reshape.go
+++ matrix_reshape/matrix_reshape.go
@@ -1,17 +1,33 @@
 package main
 
+import "fmt"
+
 func matrixReshape(nums [][]int, r int, c int) [][]int {
-	row := len(nums)
-	column := len(nums[0])
+	row, column := len(nums), len(nums[0])
+
 	if row * column != r * c {
 		return nums
 	}
 
-	new_nums := make([]int, row * column)
-	
+	res, res_row, cindex := [][]int{}, []int{}, 0
 
+	for i := 0; i < row; i++ {
+		for j := 0; j < column; j++ {
+			res_row = append(res_row, nums[i][j])
+			if cindex++; cindex == c {
+				res = append(res, res_row)
+				res_row = []int{}
+				cindex = 0
+			}
+		}
+	}
+	
+	return res
 }
 
 func main() {
-	
+	nums := [][]int{{1, 2}, {3, 4}}
+	r, c := 2, 2
+	res := matrixReshape(nums, r, c)
+	fmt.Println(res)
 }
