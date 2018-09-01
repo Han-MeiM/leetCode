c8b44a541fc7e665a1b897530eedeca419b86544
han-meim <969437631@qq.com>
2018-09-01 16:02:26 +0800

    添加另一种解题方式

--- length_of_longest_substring/length_of_longest_substring.go
+++ length_of_longest_substring/length_of_longest_substring.go
@@ -1,4 +1,4 @@
-package length_of_longest_substring
+package main
 
 import (
 	"fmt"
@@ -33,8 +33,31 @@ func in_str(s string, one int32) bool {
 	return result
 }
 
+func imooc_programme(s string) int {
+	lastOccurred := make(map[byte]int)
+	start := 0
+	maxLength := 0
+
+	for i, ch := range []byte(s) {
+		// 当该字符串最后出现的位置在 start 和 i 之间时
+		if lastI, ok := lastOccurred[ch]; ok && lastI >= start {
+			start = lastI + 1
+		}
+
+		// 判断当前子串长度是否大于之前的最长子串长度
+		if i - start + 1 > maxLength {
+			maxLength = i - start + 1
+		}
+
+		lastOccurred[ch] = i
+	}
+
+	return maxLength
+}
+
 func main() {
 	str := "abcabcbb"
 	res := lengthOfLongestSubstring(str)
 	fmt.Println(res)
+	fmt.Println(imooc_programme(str))
 }
