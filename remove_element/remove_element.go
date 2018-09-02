c2ba01be3fd685bf1db4c11919ca733e1e86cb2a
han-meim <969437631@qq.com>
2018-09-02 13:51:32 +0800

    移除元素

--- remove_element/remove_element.go
+++ remove_element/remove_element.go
@@ -1,9 +1,21 @@
 package main
 
-func removeElement(nums []int, val int) int {
+import "fmt"
 
+func removeElement(nums []int, val int) int {
+	m := 0
+	for _, n := range nums {
+		if n != val {
+			nums[m] = n
+			m++
+		}
+		fmt.Println(nums)
+	}
+	return m
 }
 
 func main() {
-	
+	nums, val := []int{3,2,2,3}, 3
+	res := removeElement(nums, val)
+	fmt.Println(nums, res)
 }
