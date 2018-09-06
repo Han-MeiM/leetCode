41d18396f9999db1b50af305299dafb028bd5243
han-meim <969437631@qq.com>
2018-09-06 10:05:51 +0800

    单调数列

--- Monotonic Array/monotonic_array.go
+++ Monotonic Array/monotonic_array.go
@@ -5,14 +5,35 @@ import (
 )
 
 func isMonotonic(A []int) bool {
-	if len(A) == 1 {
+	length := len(A)
+	if length == 1 {
 		return true
+	}
+
+	// 1 为递增，-1 为递减
+	previous := 0
+	for i := 1; i < length; i++ {
+		curren := 0
+
+		if A[i] > A[i - 1] {
+			curren = 1
+		} else if A[i] == A[i - 1] {
+			continue
+		} else {
+			curren = -1
+		}
+
+		if previous == 0 {
+			previous = curren
+		} else if previous != curren {
+			return false
+		}
 	}
 
 	return true
 }
 
 func main() {
-	nums := []int{3,2,2,1}
+	nums := []int{1,2,1}
 	fmt.Println(isMonotonic(nums))
 }
