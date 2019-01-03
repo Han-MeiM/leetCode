266e1be11491579e203556c14fa75d5031a37028
han-meim <969437631@qq.com>
2019-01-03 13:28:45 +0800

    _004.重建二叉树

---
+++ SwordOffer/src/com/_004/Solution.java
@@ -0,0 +1,32 @@
+package com.tree._004;
+
+import com.structure.TreeNode;
+
+public class Solution {
+    public TreeNode reConstructBinaryTree(int[] pre, int[] in) {
+        return reConstructBinaryTree(pre, 0, pre.length - 1, in, 0, in.length - 1);
+    }
+
+    private TreeNode reConstructBinaryTree(int[] pre, int startPre, int endPre, int[] in, int startIn, int endIn) {
+        if (startPre > endPre || startIn > endIn) {
+            return null;
+        }
+        TreeNode root = new TreeNode(pre[startPre]);
+        for (int i = startIn; i <= endIn; i++) {
+            if (in[i] == pre[startPre]) {
+                root.left = reConstructBinaryTree(pre, startPre + 1, startPre + i - startIn, in, startIn, i - 1);
+                root.right = reConstructBinaryTree(pre, i - startIn + startPre + 1, endPre, in, i + 1, endIn);
+                break;
+            }
+        }
+        return root;
+    }
+
+    public static void main(String[] args) {
+        int[] pre = {1, 2, 4, 7, 3, 5, 6, 8};
+        int[] in = {4, 7, 2, 1, 5, 3, 8, 6};
+        Solution solution = new Solution();
+        TreeNode res = solution.reConstructBinaryTree(pre, in);
+        TreeNode.print(res);
+    }
+}
