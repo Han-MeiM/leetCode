321438a1c0705a321acd169a2147fd98f8062134
han-meim <969437631@qq.com>
2019-01-03 11:50:06 +0800

    ListNode struct

---
+++ SwordOffer/src/com/structure/ListNode.java
@@ -0,0 +1,39 @@
+package com.structure;
+
+public class ListNode {
+
+    public int      val;
+    public ListNode next;
+
+    public ListNode(int x) {
+        val = x;
+    }
+
+    public static ListNode createTestData(String data) {
+        if (data.equals("[]")) return null;
+        data = data.substring(1, data.length() - 1);
+        String[] split = data.split(",");
+        int len = split.length;
+        ListNode[] listNode = new ListNode[len + 1];
+        listNode[0] = new ListNode(Integer.valueOf(split[0]));
+        for (int i = 1; i < len; i++) {
+            listNode[i] = new ListNode(Integer.valueOf(split[i]));
+            listNode[i - 1].next = listNode[i];
+        }
+        return listNode[0];
+    }
+
+    public static void print(ListNode listNode) {
+        if (listNode == null) {
+            System.out.println("null");
+            return;
+        }
+        StringBuilder str = new StringBuilder("[" + String.valueOf(listNode.val));
+        ListNode p = listNode.next;
+        while (p != null) {
+            str.append(",").append(String.valueOf(p.val));
+            p = p.next;
+        }
+        System.out.println(str.append("]"));
+    }
+}
