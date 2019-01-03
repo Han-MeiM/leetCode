feb9dcfc008a407393dd735142925dd8dea8c010
han-meim <969437631@qq.com>
2019-01-03 13:28:56 +0800

    _005.用两个栈实现队列

---
+++ SwordOffer/src/com/_005/Solution.java
@@ -0,0 +1,43 @@
+package com.stack._005;
+
+import java.util.Stack;
+
+public class Solution {
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+
+    public void push(int node) {
+        stack1.push(node);
+    }
+
+    public int pop() {
+        if (stack1.empty() && stack2.empty()) {
+            throw new RuntimeException("Queue is empty!");
+        }
+        // 当之前压入 2 的数据弹出完毕后，再重新将 1 中的数据压入 2
+        if (stack2.empty()) {
+            while (!stack1.empty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.push(1);
+        solution.push(2);
+        solution.push(3);
+        solution.push(4);
+        solution.push(5);
+        System.out.println(solution.pop());
+        System.out.println(solution.pop());
+        System.out.println(solution.pop());
+        System.out.println(solution.pop());
+        solution.push(6);
+        solution.push(7);
+        solution.push(8);
+        System.out.println(solution.stack1);
+        System.out.println(solution.stack2);
+    }
+}
