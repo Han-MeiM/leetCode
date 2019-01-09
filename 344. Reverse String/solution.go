087e91ae1f59641c0dff37f45860f99d564f904c
han-meim <969437631@qq.com>
2019-01-09 17:16:50 +0800

    344.反转字符串

--- 344. Reverse String/solution.go
+++ 344. Reverse String/solution.go
@@ -13,14 +13,23 @@ import "fmt"
 func reverseString(s string) string {
 	l := len(s)
 	res := make([]byte, 0)
-	for i:=l-1;i>=0;i-- {
+	for i := l - 1; i >= 0; i-- {
 		res = append(res, s[i])
 	}
 	return string(res)
 }
 
+func reverseStringPlanB(s string) string {
+	l := len(s)
+	res := make([]byte, l)
+	for i := 0; i <= l/2; i++ {
+		res[i], res[l-i-1] = s[l-i-1], s[i]
+	}
+	return string(res)
+}
+
 func main() {
-	str := "hello"
-	res := reverseString(str)
+	str := "asdfgh"
+	res := reverseStringPlanB(str)
 	fmt.Println(res)
 }
